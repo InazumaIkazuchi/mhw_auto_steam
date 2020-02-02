a4cee9f6c6b0ffc323d132c5e9ed8473c8b28fce
InazumaIkazuchi <44260310+InazumaIkazuchi@users.noreply.github.com>
2020-02-02 04:15:43 -0800

    Create auto.py

---
+++ auto.py
@@ -0,0 +1,46 @@
+import keyboard
+import time
+import sys
+import threading
+
+import argparse
+parser = argparse.ArgumentParser(description='Automatically press space key')
+args = parser.parse_args()
+
+RUNNING = True
+	
+# press space every 10 seconds, to deal with the cut-scenes
+def timer_runner():
+	global RUNNING
+	while RUNNING:
+		time.sleep(10)
+		keyboard.release('space')
+		keyboard.press('space')
+	
+# listener used for termination
+def listener_runner():
+	global RUNNING
+	RUNNING = False
+	keyboard.release('space')
+	
+timer = threading.Thread(target=timer_runner)
+listener = threading.Thread(target=listener_runner)
+
+print('='*20)
+print('\n请在5秒以内切到游戏内蒸汽机画面……按Q结束\n')
+print('Please switch to your game window in 5 seconds...Press \'Q\' to end\n')
+print('五秒内で蒸気機関管理所に行ってください。「Q」を押してこのプログラムを止める事ができます。')
+
+listener.start()
+# wait 5 seconds before starting, give the user some time to switch the game window
+time.sleep(5)
+
+if RUNNING:
+	timer.start()
+	keyboard.press('space')
+	# block until 'Q' is pressed
+	keyboard.wait('q')
+	timer.join()
+	
+listener.join()
+
