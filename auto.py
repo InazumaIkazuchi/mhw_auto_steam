f9ee7ad7f5c4e19c45764f88b595a7a9ac42893f
InazumaIkazuchi <44260310+InazumaIkazuchi@users.noreply.github.com>
2020-02-29 18:14:45 -0800

    Fix issue that the script stops after a cut scene.

--- auto.py
+++ auto.py
@@ -10,33 +10,34 @@ args = parser.parse_args()
 RUNNING = True
 	
 # press space every 10 seconds, to deal with the cut-scenes
-def timer_runner():
+def runner_f():
 	global RUNNING
 	while RUNNING:
-		time.sleep(10)
-		keyboard.release('space')
 		keyboard.press('space')
-	
-# used for termination
-def stop_running(args):
+		time.sleep(10)
+		keyboard.release('space')  
+		time.sleep(0.1)
+		keyboard.press_and_release('space')
+		keyboard.press('space')
+		
+def listener_f():
 	global RUNNING
+	keyboard.wait('q')
 	RUNNING = False
-	keyboard.release('space')
-	
-timer = threading.Thread(target=timer_runner)
+
+runner = threading.Thread(target=runner_f)
+listener = threading.Thread(target=listener_f)
 
 print('='*20)
 print('\n请在5秒以内切到游戏内蒸汽机画面……按Q结束\n')
 print('Please switch to your game window in 5 seconds...Press \'Q\' to end\n')
 print('五秒内で蒸気機関管理所に行ってください。「Q」を押してこのプログラムを止める事ができます。')
 
-keyboard.on_press_key('q', stop_running)
+#keyboard.on_press_key('q', stop_running)
 # wait 5 seconds before starting, give the user some time to switch the game window
 time.sleep(5)
 
-if RUNNING:
-	timer.start()
-	keyboard.press('space')
-	# block until 'Q' is pressed
-	keyboard.wait('q')
-	timer.join()
+listener.start()
+runner.start()
+listener.join()
+runner.join()
